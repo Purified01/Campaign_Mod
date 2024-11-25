0da6eb15a26162021d0b03d5b615d82ded50cb40
Purified01 <purifierecho@gmail.com>
2024-11-24 17:15:02 -0800

    streamline loading missions

--- Data/SCRIPTS/STORY/Custom_Campaign_Novus_M2.lua
+++ Data/SCRIPTS/STORY/Custom_Campaign_Novus_M2.lua
@@ -136,6 +136,7 @@ function Thread_Mission_Start()
 	Lock_Controls(0)
 	End_Cinematic_Camera()
         
+	Create_Thread("Thread_Mission_Complete")
     Show_Objective_A()
     Create_Thread("Aliens_Attack_Base")
 
